9db5e1ea7ec2c9b0aab87ff0145ec1be2a00d235
thiagoalmeidadon <thiagodons@gmail.com>
2022-01-28 17:00:35 -0300

    implementando resolver

--- dos_in_c.c
+++ dos_in_c.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <arpa/inet.h>
 
 int main(int argc, char *argv[]) {
-    printf("testando");
-    return 0;
+    int meuSocket;
+    int conecta;
+
+    struct sockaddr_in alvo;
+    struct hostent *host = gethostbyname(argv[1]);
+
+    meuSocket = socket(AF_INET, SOCK_STREAM, 0);
+    
+
+    alvo.sin_family = AF_INET;
+    alvo.sin_port = htons(21);
+    alvo.sin_addr.s_addr = inet_addr(host);
+
+    conecta = connect(meuSocket, (struct sockaddr *)&alvo, sizeof alvo);
+
+    if ( conecta == 0 ) {
+        printf("Aberta");
+        close(meuSocket);
+        close(conecta);
+    } else {
+         printf("Fechado");
+    }
+
+   
+
 }
