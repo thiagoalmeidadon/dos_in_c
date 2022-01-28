d2842beb4ba9d561cf88ee54f6b8b6fec14985b9
thiagoalmeidadon <thiagodons@gmail.com>
2022-01-28 17:23:35 -0300

    fix resolvendo dns pra ip

--- dos_in_c.c
+++ dos_in_c.c
@@ -4,27 +4,30 @@
 #include <arpa/inet.h>
 
 int main(int argc, char *argv[]) {
+    
     int meuSocket;
     int conecta;
 
     struct sockaddr_in alvo;
+    // resolvendo dns
     struct hostent *host = gethostbyname(argv[1]);
-
+    // socket
     meuSocket = socket(AF_INET, SOCK_STREAM, 0);
-    
-
     alvo.sin_family = AF_INET;
     alvo.sin_port = htons(21);
-    alvo.sin_addr.s_addr = inet_addr(host);
+    // inet_ntoa escrever ip legivel
+    alvo.sin_addr.s_addr = inet_addr( inet_ntoa( *((struct in_addr * ) host->h_addr) ) );
+
+    printf(" %s resolvido: %s \n ", argv[1] ,inet_ntoa( *((struct in_addr * ) host->h_addr) ) );
 
     conecta = connect(meuSocket, (struct sockaddr *)&alvo, sizeof alvo);
 
     if ( conecta == 0 ) {
-        printf("Aberta");
+        printf("Aberta \n");
         close(meuSocket);
         close(conecta);
     } else {
-         printf("Fechado");
+         printf("Fechado \n ");
     }
 
    
